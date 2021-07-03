4e8161e0cbc938ce83692ca21b841ff4f4631116
Joshua Dean <82895261+dean-joshua@users.noreply.github.com>
2021-07-03 13:50:14 -0600

    Added Folder of Godot Project

---
+++ Game Platform/Scenes/MenuButtons.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func StartPressed():
+	get_node("Start").move(Vector2(-576,0))
+	get_node("Difficulty").move(Vector2(0,0))
+
+
+func _on_Back_pressed():
+	get_node("Start").move(Vector2(0,0))
+	get_node("Difficulty").move(Vector2(576,0))
+
+
+func _on_Easy_pressed():
+	get_tree().change_scene("res://Scenes/Main.tscn")
